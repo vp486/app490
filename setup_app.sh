0f2066902fa321f949002d20fcde6dcb48d20587
vp486 <54951435+vp486@users.noreply.github.com>
2020-05-13 22:16:31 -0400

    Create setup_app.sh

---
+++ setup_app.sh
@@ -0,0 +1,18 @@
+sudo apt-get upgrade -y
+sudo apt-get update -y
+sudo apt install -y php php-mbstring php-bcmath composer rsyslog git
+sudo apt install -y apache2
+
+cd /var/www/html
+
+sudo mkdir FrontEnd
+
+#Basic rabbitmq setup files in be cloned. 
+
+git clone https://github.com/vp486/IT490.git
+
+cd FrontEnd
+
+sudo apt install composer
+composer require php-amqplib/php-amqplib
+composer install
